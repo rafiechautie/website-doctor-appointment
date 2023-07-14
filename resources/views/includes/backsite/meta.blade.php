e541766d1f35794ed7f40a7b840f58392508bd1f
rafiechautie <rafiqauti13@gmail.com>
2023-07-14 19:51:13 +0700

    add meta, style, and script on backsite

---
+++ resources/views/includes/backsite/meta.blade.php
@@ -0,0 +1,17 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+{{-- IE=edge digunakan agar sistem kita kompatible di browser edge  --}}
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+
+{{-- seo setting base --}}
+seo setting bagusnya diletakkan di frontsite, di backsite juga bagus diletakkan
+
+{{-- jelasin kalau web ini adalah tentang Meet Your Doctor. Trusted & Professional. --}}
+<meta name="description" content="{{ 'Meet Your Doctor. Trusted & Professional.' }}">
+{{-- sistem akan muncul jika salah satu keyword dimasukkan sama user saat melakukan pencarian website  --}}
+<meta key="keywords" content="{{ 'doctor, meetdoctor, patien, appointment, trusted & professional' }}">
+{{-- author adalah pembuat atau nama perusahaan yang mengembangkan sistem  --}}
+<meta name="author" content="{{ 'Meet Doctor' }}">
+
+<!-- CSRF Token -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
